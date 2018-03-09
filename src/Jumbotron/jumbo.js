b8b11971126b406865baf2c7af7e199da621da67
Megane <gr.benedith@gmail.com>
2018-03-08 23:36:37 -0400

    add style

--- src/Jumbotron/jumbo.js
+++ src/Jumbotron/jumbo.js
@@ -4,18 +4,13 @@ import fond from '../Images/resto.jpeg';
 
 const divStyle = {
 	backgroundImage: `url(${fond})`,
-	height: '500px',
-	backgroundSize: 'cover',
-	backgroundPosition: '2% 75%',
-	backgroundColor: 'rgb(128, 118, 118)',
-	backgroundBlendMode: 'multiply',
-	width: 'auto',
+	
 };
 class Jumbotron extends Component {
 	render() {
 		return (
 			<div className="JumbotronResto">
-				<div style={divStyle}>
+				<div className="styleImag" style={divStyle}>
 					<h1 className="TitreJumbotron">BAISERS DE FRAMBOISES</h1>
 					<h2 className="SousTitreJumbotron">PARFUMÉS À LA FRAISE</h2>
 				</div>
