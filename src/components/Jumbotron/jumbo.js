ee888bddee1871936ba1e77c6524ac9a838bb2bf
Kenpachi971 <mrcapharsie.m@gmail.com>
2018-03-09 13:01:16 -0400

    Header et Jumbo

--- src/components/Jumbotron/jumbo.js
+++ src/components/Jumbotron/jumbo.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import './jumbo.scss';
 import fond from '../Images/resto.jpeg';
 
-
-
 const divStyle = {
 	backgroundImage: `url(${fond})`,
-	
 };
 class Jumbotron extends Component {
 	render() {
 		return (
 			<div className="JumbotronResto">
 				<div className="styleImag" style={divStyle}>
-					<h1 className="TitreJumbotron">TOUT DANS LES PAPILLES</h1>
-					<h2 className="SousTitreJumbotron">LE PLAISIR GUSTATIVE</h2>
+					<h1 className="TitreJumbotron">Tout dans les papilles</h1>
+					<h2 className="SousTitreJumbotron">Le plaisir gustatif</h2>
 				</div>
 			</div>
 		);
